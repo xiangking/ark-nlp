1d7df570bfa34f317d69711aa5844409ccc26cd5
xiangking <wx15857152367@163.com>
2021-03-02 09:12:59 +0800

    update: add predictor

---
+++ ark_nlp/factory/predictor/_classification.py
@@ -0,0 +1,144 @@
+"""
+# Copyright 2021 Xiang Wang, Inc. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at 
+# http://www.apache.org/licenses/LICENSE-2.0
+
+Author: Xiang Wang, xiangking1995@163.com
+Status: Active
+"""
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable, grad
+from torch.utils.data import DataLoader, Dataset
+import torch.nn.functional as F
+
+import tqdm
+from tqdm import tqdm
+import sklearn.metrics as sklearn_metrics
+
+
+class Predictor(object):
+    def __init__(
+        self, 
+        module, 
+        tokernizer, 
+        cat2id
+    ):
+
+        self.module = module
+        self.cat2id = cat2id
+        self.tokenizer = tokernizer
+        self.device = list(self.module.parameters())[0].device
+
+        self.id2cat = {}
+        for cat_, idx_ in self.cat2id.items():
+            self.id2cat[idx_] = cat_
+
+    def _convert_to_transfomer_ids(
+        self, 
+        text
+    ):
+        input_ids = self.tokenizer.text_to_sequence(text)  
+        input_ids, input_mask, segment_ids = input_ids
+
+        features = {
+                'input_ids': input_ids, 
+                'attention_mask': input_mask, 
+                'token_type_ids': segment_ids
+            }
+        return features
+
+    def _convert_to_vanilla_ids(
+        self, 
+        text
+    ):
+        input_ids = self.tokenizer.text_to_sequence(text)   
+
+        features = {
+                'input_ids': input_ids
+            }
+        return features
+
+    def _get_input_ids(
+        self, 
+        text
+    ):
+        if tokenizer.tokenizer_type == 'vanilla':
+            return self._convert_to_vanilla_ids(text)
+        elif tokenizer.tokenizer_type == 'transfomer':
+            return self._convert_to_transfomer_ids(text)
+        elif tokenizer.tokenizer_type == 'customized':
+            features = self._convert_to_customized_ids(tokenizer)
+        else:
+            raise ValueError("The tokenizer type does not exist") 
+
+    def _get_module_one_sample_inputs(
+        self, 
+        features
+    ):
+        return {col: inputs[col].unsqueeze(0).to(self.device) for col in features}
+
+    def predict_one_sample(
+        self, 
+        text='', 
+        topk=None
+    ):
+        if topk == None:
+            topk = len(self.cat2id)
+
+        features = self._get_input_ids(input_str)
+        
+        with torch.no_grad():
+            inputs = self._get_module_one_sample_inputs(features):
+            logit = self.model.module(**inputs)
+            logit = torch.nn.functional.softmax(logit, dim=1)
+
+        probs, indices = logit.topk(topk, dim=1, sorted=True)
+        
+        res_list = []
+        for pred_, prob_ in zip(indices.cpu().numpy()[0], probs.cpu().numpy()[0].tolist()):
+            res_list.append([self.id2cat[pred_], prob_])
+
+        return res_list
+
+    def _get_module_batch_inputs(
+        self, 
+        features
+    ):
+        return {col: inputs[col].to(self.device) for col in self.inputs_cols}
+
+    def predict_batch(
+        self, 
+        test_data, 
+        batch_size=16, 
+        shuffle=False, 
+        is_proba=False
+    ):
+        self.inputs_cols = test_data.dataset_cols
+        
+        preds = []
+        probas=[]
+        
+        self.module.eval()
+        generator = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+        
+        for step, inputs in enumerate(generator):
+            inputs = self._get_module_batch_inputs(inputs)
+            
+            logits = self.module(**inputs)
+
+            preds.extend(torch.max(logits, 1)[1].cpu().numpy())  
+            if is_proba:
+                probas.extend(F.softmax(logits, 1).cpu().detach().numpy())  
+
+        if is_prob:
+            return preds, probas
+        
+        return preds
