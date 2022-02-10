c147ec88d50656465ad2d01ad1b79b929460e343
jimme0421 <jimme.shen123@gamil.com>
2022-02-10 17:00:24 +0800

    feature (model) : 增加EfficientGlobalPointer模块

--- ark_nlp/model/ner/global_pointer_bert/global_pointer_bert.py
+++ ark_nlp/model/ner/global_pointer_bert/global_pointer_bert.py
@@ -1,6 +1,6 @@
 from transformers import BertModel
 from ark_nlp.nn.base.bert import BertForTokenClassification
-from ark_nlp.nn.layer.global_pointer_block import GlobalPointer
+from ark_nlp.nn.layer.global_pointer_block import GlobalPointer, EfficientGlobalPointer
 
 
 class GlobalPointerBert(BertForTokenClassification):
@@ -57,5 +57,62 @@ class GlobalPointerBert(BertForTokenClassification):
         sequence_output = outputs[-1]
 
         logits = self.global_pointer(sequence_output, mask=attention_mask)
+
+        return logits
+
+class EfficientGlobalPointerBert(BertForTokenClassification):
+    """
+    EfficientGlobalPointer + Bert 的命名实体模型
+
+    Args:
+        config: 模型的配置对象
+        bert_trained (:obj:`bool`, optional): 预训练模型的参数是否可训练
+
+    Reference:
+        [1] https://www.kexue.fm/archives/8877
+        [2] https://github.com/suolyer/PyTorch_BERT_Biaffine_NER
+    """  # noqa: ignore flake8"
+
+    def __init__(
+        self,
+        config,
+        encoder_trained=True,
+        head_size=64
+    ):
+        super(EfficientGlobalPointerBert, self).__init__(config)
+
+        self.num_labels = config.num_labels
+
+        self.bert = BertModel(config)
+
+        for param in self.bert.parameters():
+            param.requires_grad = encoder_trained
+
+        self.efficient_global_pointer = EfficientGlobalPointer(
+            self.num_labels,
+            head_size,
+            config.hidden_size
+        )
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        **kwargs
+    ):
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            return_dict=True,
+            output_hidden_states=True
+        ).hidden_states
+
+        sequence_output = outputs[-1]
+
+        logits = self.efficient_global_pointer(sequence_output, mask=attention_mask)
 
         return logits
