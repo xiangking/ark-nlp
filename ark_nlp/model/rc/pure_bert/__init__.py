cd482d2968ee285a96f62ff84a50af8cd3d29aeb
jimme0421 <jimme.shen123@gamil.com>
2022-07-14 10:15:38 +0800

    chore(*): 修改名称

--- ark_nlp/model/rc/pure_bert/__init__.py
+++ ark_nlp/model/rc/pure_bert/__init__.py
@@ -1,5 +1,5 @@
-from ark_nlp.model.rc.pure_bert.pure_relation_extraction_dataset import PUREREDataset
-from ark_nlp.model.rc.pure_bert.pure_relation_extraction_dataset import PUREREDataset as Dataset
+from ark_nlp.model.rc.pure_bert.pure_relation_classification_dataset import PUREREDataset
+from ark_nlp.model.rc.pure_bert.pure_relation_classification_dataset import PUREREDataset as Dataset
 
 from ark_nlp.processor.tokenizer.transfomer import SpanTokenizer as Tokenizer
 from ark_nlp.processor.tokenizer.transfomer import SpanTokenizer as PURERETokenizer
@@ -13,8 +13,8 @@ from ark_nlp.model.rc.pure_bert.pure_bert import PUREBert as Module
 from ark_nlp.factory.optimizer import get_default_bert_optimizer as get_default_model_optimizer
 from ark_nlp.factory.optimizer import get_default_bert_optimizer as get_default_casrel_bert_optimizer
 
-from ark_nlp.model.rc.pure_bert.pure_relation_extraction_task import PURERETask as Task
-from ark_nlp.model.rc.pure_bert.pure_relation_extraction_task import PURERETask as PUREEBert
+from ark_nlp.model.rc.pure_bert.pure_relation_classification_task import PURERETask as Task
+from ark_nlp.model.rc.pure_bert.pure_relation_classification_task import PURERETask as PUREEBert
 
-from ark_nlp.model.rc.pure_bert.pure_relation_extraction_predictor import PUREREPredictor as Predictor
-from ark_nlp.model.rc.pure_bert.pure_relation_extraction_predictor import PUREREPredictor as PUREREPredictor
+from ark_nlp.model.rc.pure_bert.pure_relation_classification_predictor import PUREREPredictor as Predictor
+from ark_nlp.model.rc.pure_bert.pure_relation_classification_predictor import PUREREPredictor as PUREREPredictor
